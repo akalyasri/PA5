86b23a7e55318a48fdee4062b1cfc52483bd6cb9
Akalya Sridharan <akalya.s.sri@gmail.com>
2023-03-07 18:34:15 -0800

    created outline for functions enqueue, dequeue, and printQueue

--- PA5/queue.hpp
+++ PA5/queue.hpp
@@ -4,6 +4,35 @@ class Queue {
 
 public: // Member functions
 
+	Queue() {
+		pTail = nullptr;
+		pHead = nullptr;
+	}
+
+	void enqueue(int cusNum, int serTime, int totalT) { // insert
+
+
+	}
+
+	bool dequeue(int& pcNum, int& psTime, int& ptTime) { // delete head and returning whatever values it holds
+
+		if (pHead == NULL) {
+			return false;
+
+		}
+		else {
+			// TBD
+		}
+
+	}
+
+	void printQueue() {
+
+		//	TBD
+
+	}
+	
+
 private:
 	QueueNode* pHead;
 	QueueNode* pTail;
